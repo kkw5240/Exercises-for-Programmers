ab763be3a88acc006f636fbbb618c8a85f568c33
kkw5240 <kkw5240@gmail.com>
2023-01-20 23:06:20 +0900

    feature(Ex03): 연습문제 3. 따옴표 출력
    큰따옴표는 문자열의 시작과 끝을 나타내는 용도로 자주 사용되지만, 가끔은 확장문자(escape character)를 사용하여 따옴표 자체를 출력할 일도 생긴다. 인용구와 그 말을 한 사람을 입력 받는 프로그램을 작성하라. 인용구와 사람 이름은 다음의 출력 예와 같이 나타내보자.
    
    # 출력 예
    ```shell
    What is the quote? These aren't the droids you're looking for
    Who said it? Obi-Wan Kenobi
    Obi-Wan Kenobi says, "These aren't the droids you're looking for."
    ```
    
    # 제약 조건
    - 한 개의 출력문만 사용하여 결과를 출력할 것. 이때 따옴표를 출력하기 위해 적절한 확장문자를 사용해야 한다.
    - 만일 사용하는 프로그래밍 언어가 문자열 보간(String Interpolation)이나 문자열 대체(String Substitution)를 지원하는 경우라도 이 기능들을 사용하지 말고 그냥 문자열 연결(String Concatenation)을 사용할 것
    
    # 도전 과제
    - 7장에서 데이터 리스트에 대해서도 연습하게 될 것이다. 앞의 프로그램을 수정하여 사용자로부터 입력을 받는 대신 인용구와 이와 관련된 내용(사람 이름)을 담는 자료 구조를 만들어 모든 내용을 앞의 출력 예와 같이 나타내보자. 맵 형태의 배열을 사용하면 좋을 것이다.

--- ex03/java/kkw5240.java
+++ ex03/java/kkw5240.java
@@ -1,17 +1,76 @@
 package ex03.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Ex03 {
+    private static Scanner scanner;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
 
+        Quotes quotes = new Quotes();
+
+        while (isEmptyInput()) {
+            Quote quote = makeQuote();
+            quote.printQuote();
+
+            quotes.add(quote);
+        }
+        quotes.printQuotes();
+
+        scanner.close();
+    }
+
+    private static Quote makeQuote() {
+        String message = getMessage();
+        String person = getPerson();
+
+        return new Quote(person, message);
+    }
+
+    private static boolean isEmptyInput() {
+        return scanner.nextLine().isEmpty();
+    }
+
+    private static String getMessage() {
         System.out.print("What is the quote? ");
-        String quote = scanner.nextLine();
+        return scanner.nextLine();
+    }
 
+    private static String getPerson() {
         System.out.print("Who said it? ");
-        String person = scanner.nextLine();
+        return scanner.nextLine();
+    }
 
-        System.out.println(person + " says, \"" + quote + ".\"");
+    private static class Quote {
+        private final String person;
+        private final String message;
+
+        public Quote(String person, String message) {
+            this.person = person;
+            this.message = message;
+        }
+
+        public void printQuote() {
+            System.out.println(this.person + " says, \"" + this.message + ".\"");
+        }
+    }
+
+    private static class Quotes {
+        private final List<Quote> quotes;
+
+        public Quotes() {
+            this.quotes = new ArrayList<>();
+        }
+
+        public void add(Quote quote) {
+            this.quotes.add(quote);
+        }
+
+        public void printQuotes() {
+            this.quotes.forEach(Quote::printQuote);
+            System.out.println();
+        }
     }
 }
