8e63f622b2ca0321f26d09c9a89e8e80d508ac20
kkw5240 <kkw5240@gmail.com>
2023-01-20 19:27:27 +0900

    feature(Ex01): 연습문제 1. 인사하기
    “Hello World” 프로그램은 프로그래밍 언어를 배울 때 가장 먼저 작성
    하는 프로그램이다. 하지만 이 프로그램에는 입력을 받는 부분이 없다.
    그래서 여기에서는 여러분의 이름을 입력 받아 이름을 이용하여 인사
    말을 출력하는 프로그램을 작성해보자.
    
    # 출력 예
    ```shell
    What is your name? Brian
    Hello, Brian, nice to meet you!
    ```
    
    # 제약 조건
    - 입력 부분, 문자열 연결(String concatenation) 부분, 출력 부분을 별도로 작성할 것
    
    # 도전 과제
    - 변수를 사용하지 않는 새로운 버전을 작성하라.
    - 사람들마다 서로 다른 인사말이 나타나도록 프로그램을 작성하라. 이 도전 과제는 4장과 7장의 예제를 해결한 다음에 도전하면 더 좋다

--- ex01/java/kkw5240.java
+++ ex01/java/kkw5240.java
@@ -30,6 +30,33 @@ class Ex01 {
     }
 
     private static String getMessage(String name) {
-        return "Hello, " + name + ", nice to meet you!";
+        return "Hello, " + name + ", " + getGreetingMessage(name);
+    }
+
+    private static String getGreetingMessage(String name) {
+        GreetingType greetingType = GreetingType.valueOf(
+                String.valueOf(
+                        name.toUpperCase().charAt(0)
+                )
+        );
+
+        return greetingType.getGreetingMessage();
+    }
+
+    enum GreetingType {
+        A("good afternoon!"), B("nice to meet you!"), C("Come In!"), D("how was your day?"), E("good evening."), F("have fun~"), G("good to see you!"),
+        H("have a good day!"), I("how are you these days?"), J("jolly your day."), K("what has kept you so busy?"), L("long time no see!"), M("good morning!"), N("good night!"),
+        O("how is your business going?"), P("see you again."), Q("how are you doing?"), R("how are you?"), S("what a small world!"), T("Hello"), U("fancy meeting you here!"),
+        V("wakanda forever!"), W("what's up?"), X("xxxx"), Y("Hello"), Z("i never expected to see you here!");
+
+        private final String greetingMessage;
+
+        GreetingType(String greetingMessage) {
+            this.greetingMessage = greetingMessage;
+        }
+
+        public String getGreetingMessage() {
+            return this.greetingMessage;
+        }
     }
 }
