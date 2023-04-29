6463202b6d813c11ea9248c0536101e22f5445c8
kkw5240 <kkw5240@gmail.com>
2023-04-29 23:10:37 +0900

    feature(java/Ex07): 연습문제 7. 직사각형 방의 면적
    - 방의 면적을 계산하는 프로그램을 작성
    - 방의 길이와 폭을 피트 단위로 입력받아 제곱피트와 제곱미터로 면적을 나타내라
    
    ## 출력 예
    ```text
    What is the length of room in feet? 15
    What is the width of the room in feet? 20
    You entered dimensions of 15 feet by 20 feet
    The area is
    300 square feet
    27.871 square meters
    ```
    > 제곱피트에서 제곱미터로 변환하는 식은 다음과 같다.
    > m^2 = f^2 * 0.09290304
    
    ## 제약조건
    - 출력문과 계산부분을 분리할 것
    - 상수를 사용하여 변환 상수를 저장할 것
    
    ## 도전과제
    - 입력 값으로 숫자만 받을 수 있도록 프로그램을 수정(숫자가 입력될 때까지 진행되지 않도록)
    - 입력 값이 피트 단위인지 미터 단위인지를 선택하는 새로운 버전을 만들기
    - 이 프로그램을 GUI 버전으로 구현, 입력 값이 변경되는 즉시 바로 결과 업데이트

--- ex07/java/kkw5240.java
+++ ex07/java/kkw5240.java
@@ -4,30 +4,49 @@ import java.util.Scanner;
 
 class Ex07 {
     private final static double SQUARE_FEET_TO_SQUARE_METER = 0.09290304;
-    private double length;
-    private double width;
-    private double areaSquareFeet;
-    private double areaSquareMeter;
 
     public static void main(String[] args) {
+        double length = promptLength();
+        double width = promptWidth();
+
+        double areaSquareFeet = getArea(length, width);
+        double areaSquareMeter = convertFeetToMeter(areaSquareFeet);
+
+        System.out.printf("""
+                You entered dimensions of %.1f feet by %.1f feet
+                The area is
+                %.3f square feet
+                %.3f square meters.
+                %n""", length, width, areaSquareFeet, areaSquareMeter);
+    }
+
+    private static double promptLength() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("What is the length of the room in feet? ");
         double length = scanner.nextDouble();
 
+        scanner.close();
+
+        return length;
+    }
+
+    private static double promptWidth() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("What is the width of the room in feet? ");
         double width = scanner.nextDouble();
 
-        double areaSquareFeet = length * width;
-        double areaSquareMeter = areaSquareFeet * SQUARE_FEET_TO_SQUARE_METER;
+        scanner.close();
 
-        String message = String.format("""
-                You entered dimensions of %.1f feet by %.1f feet
-                The area is
-                %.3f square feet
-                %.3f square meters.
-                """, length, width, areaSquareFeet, areaSquareMeter);
+        return width;
+    }
 
-        System.out.println(message);
+    private static double getArea(double length, double width) {
+        return length * width;
+    }
+
+    private static double convertFeetToMeter(double areaSquareFeet) {
+        return areaSquareFeet * SQUARE_FEET_TO_SQUARE_METER;
     }
 }
