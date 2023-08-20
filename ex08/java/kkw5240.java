7d7958ca705338ad23f1b9a62b1e4c36952cea7f
switThomasKim <thomas.kim@swit.io>
2023-08-20 17:27:40 +0900

    .

--- ex08/java/kkw5240.java
+++ ex08/java/kkw5240.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 class Ex08 {
     public static void main(String[] args) {
+        int people = 1, pizzas = 0, pieces = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("How many people? ");
-        int people = scanner.nextInt();
+        try {
+            System.out.printf("How many people? ");
+            people = scanner.nextInt();
 
-        System.out.printf("How many pizzas do you have? ");
-        int pizzas = scanner.nextInt();
+            System.out.printf("How many pizzas do you have? ");
+            pizzas = scanner.nextInt();
 
-        System.out.printf("How many pieces are in a pizza? ");
-        int pieces = scanner.nextInt();
+            System.out.printf("How many pieces are in a pizza? ");
+            pieces = scanner.nextInt();
 
-        scanner.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
 
         System.out.printf("\n%d people with %d pizzas.\n", people, pizzas);
 
